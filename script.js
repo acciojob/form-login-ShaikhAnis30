f0f10fc8cf18753ff4aed94e9d3b68c79bffbc02
ShaikhAnis30 <anisshaikh8535@gmail.com>
2023-07-27 14:32:53 +0530

    Added

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+let inputs = document.getElementsByTagName("input");
+
 function getFormvalue() {
     //Write your code here
-
+    let firstName = inputs[0].value;
+    let lastName = inputs[1].value;
+    console.log(firstName + " " +lastName);
+    alert(`${firstName} ${lastName}`);
 }
